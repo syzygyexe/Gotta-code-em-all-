aa097d20ab2dc9079141927361d46920cf833310
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-17 16:12:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { MainPage } from "./containers/main";
 import { BrowseContainer } from "./containers/browse";
-import * as ROUTES from "./constants/routes";
-
-// RE-DO to NPM FORMAT(!!!!!!!!!!!)
-const Router = window.ReactRouterDOM.BrowserRouter;
-const Route = window.ReactRouterDOM.Route;
-const Link = window.ReactRouterDOM.Link;
-const Prompt = window.ReactRouterDOM.Prompt;
-const Switch = window.ReactRouterDOM.Switch;
-const Redirect = window.ReactRouterDOM.Redirect;
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={MainPage} />
+        <Route path='/' exact component={MainPage} />
       </Switch>
       <Switch>
-        <Route path="/browse" exact component={BrowseContainer} />
+        <Route path='/browse' exact component={BrowseContainer} />
       </Switch>
     </Router>
   );
