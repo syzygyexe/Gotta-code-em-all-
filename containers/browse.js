bb9e5abdd617321e1766b9cd9fe61b5b469528d0
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-19 16:49:48 +0900

    Update browse.js

--- containers/browse.js
+++ containers/browse.js
@@ -1,23 +1,82 @@
 import React, { useState, useEffect } from "react";
-import { Loading } from "../components";
+import { Loading, Browse, Navbar } from "../components";
+import axios from "axios";
 
 export function BrowseContainer() {
   const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonImg, setPokemonImg] = useState([]);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 2500);
+    }, 1);
   }, []);
 
-  return (
+  // useEffect(() => {
+  //   axios.get("https://pokeapi.co/api/v2/pokemon/").then((res) => {
+  //     //   res.data is all json data from the pokeapi. Inside results there is an array of diffrent data(name etc).
+  //     setPokemon(res.data.results.map((p) => p.name));
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const toArray = [];
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      console.log(res);
+      toArray.push(res.data);
+      console.log(toArray);
+      // setPokemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+    };
+    getPokemon();
+  }, []);
+
+  useEffect(() => {
+    const imageCollection = async () => {
+      const imagePromiseArr = [];
+      for (let id = 1; id <= 20; id++) {
+        imagePromiseArr.push(
+          `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+        );
+      }
+      const result = await Promise.all(imagePromiseArr);
+      setPokemonImg(result);
+    };
+    imageCollection();
+  }, []);
+
+  return loading ? (
+    <Loading />
+  ) : (
     <>
-      {/* Run Loading */}
-      {loading ? (
-        <Loading text="Loading Your Pokemon" />
-      ) : (
-        <h1>NEW PAGE NEW PAGE NEW PAGE</h1>
-      )}
+      <Loading.ReleaseBody />
+      <Navbar>
+        <Navbar.Frame>
+          <Navbar.Image
+            src={"../images/misc/about.gif"}
+            width='50px'
+            height='auto'
+          />
+          <Navbar.Button>About</Navbar.Button>
+        </Navbar.Frame>
+      </Navbar>
+      <Browse>
+        <Browse.Title>Pokepedia</Browse.Title>
+        <Browse.Frame>
+          {pokemonData.map((data, index) => (
+            <Browse.Card key={index}>
+              <Browse.CardImage src={pokemonImg[index]} />
+              <h1> {Math.round(data.weight / 4.3)} lbs</h1>
+            </Browse.Card>
+          ))}
+        </Browse.Frame>
+      </Browse>
     </>
   );
 }
+
