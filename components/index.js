26560233877cf8d4605f1eb0cc8fcea2bdcd760b
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-24 22:41:48 +0900

    Delete index.js

--- components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Main } from "./main";
-export { default as Loading } from "./loading";
-export { default as Browse } from "./browse";
-export { default as Navbar } from "./navbar";
-// export { default as Modal } from "./modal";
