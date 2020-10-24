1f15ee0fcfdede0e16d5f0de26e561b58d1ccb6e
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-24 22:07:45 +0900

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as Main } from "./main";
+export { default as Loading } from "./loading";
+export { default as Browse } from "./browse";
+export { default as Navbar } from "./navbar";
+// export { default as Modal } from "./modal";
