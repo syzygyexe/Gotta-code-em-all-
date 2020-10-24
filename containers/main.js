b849e2b28d6b25c2e72bef914b33e16bbb12f1c4
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-24 22:09:00 +0900

    Update main.js

--- containers/main.js
+++ containers/main.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Main } from "../components";
-// import * as ROUTES from "../constants/routes";
 
 export function MainPage({ children }) {
   return (
     <Main>
       <Main.Frame>
-        <Main.ButtonLink to={"/browse"}>
-          Explore Pokemon Encyclopedia
-        </Main.ButtonLink>
+        <Main.ButtonLink to={"/browse"}>EXPLORE POKEPEDIA</Main.ButtonLink>
       </Main.Frame>
     </Main>
   );
