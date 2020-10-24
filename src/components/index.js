4096a799dd28c27c69e24b8cccd62bf7d9ec05b6
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-24 22:51:56 +0900

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Main } from "./main";
 export { default as Loading } from "./loading";
 export { default as Browse } from "./browse";
 export { default as Navbar } from "./navbar";
-export { default as Modal } from "./modal";
+// export { default as Modal } from "./modal";
