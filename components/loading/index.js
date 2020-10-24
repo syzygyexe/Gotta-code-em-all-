f2163dbba1aa10134c2ba6510e3b481fe3cd7b75
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-24 22:05:35 +0900

    Update index.js

--- components/loading/index.js
+++ components/loading/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { LockBody, ReleaseBody, Runner, Background } from './styles/loading';
+import React from "react";
+import { LockBody, ReleaseBody, Runner } from "./styles/loading";
 
 export default function Loading({ src, ...restProps }) {
-    return (
-      <Runner {...restProps}>
-        <LockBody />
-      </Runner>
-    );
-  }
-  
-  Loading.ReleaseBody = function LoadingReleaseBody() {
-    return <ReleaseBody />;
-  };
+  return (
+    <Runner {...restProps}>
+      <LockBody />
+    </Runner>
+  );
+}
+
+Loading.ReleaseBody = function LoadingReleaseBody() {
+  return <ReleaseBody />;
+};
